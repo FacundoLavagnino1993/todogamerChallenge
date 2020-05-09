8b31b444eec31a04d3ebec6d8464593d22474232
Facundo Lavagnino <facundo.lavagnino@almundo.com>
2020-05-09 00:24:16 -0300

    add header

--- todosgamersAbm/src/client/main.js
+++ todosgamersAbm/src/client/main.js
@@ -1,7 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' 
+import VueRouter from 'vue-router';
+import './styles/normalize.scss';
+
+Vue.use(VueRouter); 
+
+import HeaderComponent from './components/header.vue';
+
+const routes = [{
+    path: '/',
+    component: HeaderComponent,
+  }];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  render: h => h(HeaderComponent),
+  router
+}).$mount('#app');
